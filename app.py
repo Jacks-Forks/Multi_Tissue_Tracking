e3e71261936946d594fca5d5188a2cfe26c7e60f
Brendan Murphy <thesimplekid@gmail.com>
2020-05-08 01:23:49 -0400

    upload works to proper file does not dispaly

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ dirpath = os.getcwd()
 
 logging.info(dirpath)
 
+UPLOAD_FOLDER = dirpath + "/static/uploads"
+VID_UPLOAD_FOLDER = UPLOAD_FOLDER + "/video"
+CSV_UPLOAD_FOLDER = UPLOAD_FOLDER + "/csvfiles"
+ALLOWED_EXTENSIONS = {'csv', 'mov'}
 
-UPLOAD_FOLDER = dirpath + "/static/vid"
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mov'}
+
+
 logging.info(UPLOAD_FOLDER)
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -20,11 +24,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def main():
     return render_template('index.html')
 
+def whitchFileType(filename):
+    VID_UPLOAD_FOLDER = UPLOAD_FOLDER + "/video"
+    CSV_UPLOAD_FOLDER = UPLOAD_FOLDER + "/csvfiles"
+    result = ""
+    if filename.rsplit('.', 1)[1].lower() == 'csv':
+        #UPLOAD_FOLDER = CSV_UPLOAD_FOLDER
+        result = CSV_UPLOAD_FOLDER
+    elif filename.rsplit('.', 1)[1].lower() == 'mov':
+        result = VID_UPLOAD_FOLDER
+    return result
+    #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload_vid', methods = [ 'GET', 'POST'])
-def upload_vid():
+@app.route('/uploadFile', methods = [ 'GET', 'POST'])
+def upload_file():
     if request.method == 'POST':
     # check if the post request has the file part
         if 'file' not in request.files:
@@ -37,22 +53,21 @@ def upload_vid():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
+            where_to_save = whitchFileType(file.filename)
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+            file.save(os.path.join(where_to_save, filename))
+            logging.info("here" + whitchFileType(file.filename))
+            return redirect(url_for('main'))
+            #return redirect(url_for(fileType=where_to_save, filename=filename))
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return render_template('upload.html')
+
+
+#this needs to be fixed    
+@app.route('/<fileType>/<filename>')
+def uploaded_file(fileType, filename):
+    logging.info("hell" + fileType)
+    return send_from_directory(fileType, filename)
 
 if __name__ == "__main__":
     app.run()
