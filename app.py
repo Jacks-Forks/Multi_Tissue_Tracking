e678b66c14d0d4255643ab91f2a3628083d5c9d1
Brendan Murphy <thesimplekid@gmail.com>
2020-05-08 14:35:46 -0400

    list of files can be viewed

--- app.py
+++ app.py
@@ -7,37 +7,32 @@ logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 dirpath = os.getcwd()
 
-logging.info(dirpath)
-
 UPLOAD_FOLDER = dirpath + "/static/uploads"
-VID_UPLOAD_FOLDER = UPLOAD_FOLDER + "/video"
+VIDEO_UPLOAD_FOLDER = UPLOAD_FOLDER + "/videofiles"
 CSV_UPLOAD_FOLDER = UPLOAD_FOLDER + "/csvfiles"
+
 ALLOWED_EXTENSIONS = {'csv', 'mov'}
 
-
-
-logging.info(UPLOAD_FOLDER)
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['VIDEO_FOLDER'] = VIDEO_UPLOAD_FOLDER
+app.config['CSV_FOLDER'] = CSV_UPLOAD_FOLDER
 
-logging.info("What does app print" + app.config['UPLOAD_FOLDER'])
-
-@app.route("/")
-def main():
-    return render_template('index.html')
-
-def file_type_upload_folder(filename):
-    VID_UPLOAD_FOLDER = "/video"
-    CSV_UPLOAD_FOLDER ="/csvfiles"
+def where_to_upload(filename):
     result = ""
     if filename.rsplit('.', 1)[1].lower() == 'csv':
-        result = CSV_UPLOAD_FOLDER
+        result = app.config['CSV_FOLDER']
     elif filename.rsplit('.', 1)[1].lower() == 'mov':
-        result = VID_UPLOAD_FOLDER
+        result = app.config['VIDEO_FOLDER']
     return result
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def main():
+    return render_template('index.html')
 
 @app.route('/uploadFile', methods = [ 'GET', 'POST'])
 def upload_file():
@@ -52,22 +47,25 @@ def upload_file():
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
-            whitch_file_type = file_type_upload_folder(file.filename)
-            where_to_save = app.config['UPLOAD_FOLDER'] + whitch_file_type
+            where_to_save = where_to_upload(file.filename)
             logging.info('Where does it save: ' + where_to_save)
             filename = secure_filename(file.filename)
             file.save(os.path.join(where_to_save, filename))
-            return redirect(url_for('uploaded_file', fileType = whitch_file_type, filename = filename))
+            return '''
+            <!DOCTYPE html>
+            <h1> uploaded </h1>
+            '''
 
     return render_template('upload.html')
 
+@app.route('/uploads/<filefolder>')
+def view_upload_folder(filefolder):
+    list_of_files = []
 
-#this needs to be fixed
-@app.route('/uploads/<fileType>/<filename>')
-def uploaded_file(fileType, filename):
-    path_to_file = fileType + "/" + filename
-    logging.info("path: " + path_to_file)
-    return send_from_directory(app.config['UPLOAD_FOLDER'], path_to_file)
+    for filename in os.listdir(app.config[filefolder]):
+        list_of_files.append(filename)
+
+    return render_template('viewUploads.html', value = list_of_files)
 
 if __name__ == "__main__":
     app.run()
