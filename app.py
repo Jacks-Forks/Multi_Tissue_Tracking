5f4ed74501968217804e234cd29fe89c9d59ef30
Brendan Murphy <thesimplekid@gmail.com>
2020-05-08 13:36:37 -0400

    returns the uploaded file

--- app.py
+++ app.py
@@ -20,21 +20,21 @@ logging.info(UPLOAD_FOLDER)
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+logging.info("What does app print" + app.config['UPLOAD_FOLDER'])
+
 @app.route("/")
 def main():
     return render_template('index.html')
 
-def whitchFileType(filename):
-    VID_UPLOAD_FOLDER = UPLOAD_FOLDER + "/video"
-    CSV_UPLOAD_FOLDER = UPLOAD_FOLDER + "/csvfiles"
+def file_type_upload_folder(filename):
+    VID_UPLOAD_FOLDER = "/video"
+    CSV_UPLOAD_FOLDER ="/csvfiles"
     result = ""
     if filename.rsplit('.', 1)[1].lower() == 'csv':
-        #UPLOAD_FOLDER = CSV_UPLOAD_FOLDER
         result = CSV_UPLOAD_FOLDER
     elif filename.rsplit('.', 1)[1].lower() == 'mov':
         result = VID_UPLOAD_FOLDER
     return result
-    #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -42,32 +42,32 @@ def allowed_file(filename):
 @app.route('/uploadFile', methods = [ 'GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-    # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
+
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
-            where_to_save = whitchFileType(file.filename)
+            whitch_file_type = file_type_upload_folder(file.filename)
+            where_to_save = app.config['UPLOAD_FOLDER'] + whitch_file_type
+            logging.info('Where does it save: ' + where_to_save)
             filename = secure_filename(file.filename)
             file.save(os.path.join(where_to_save, filename))
-            logging.info("here" + whitchFileType(file.filename))
-            return redirect(url_for('main'))
-            #return redirect(url_for(fileType=where_to_save, filename=filename))
+            return redirect(url_for('uploaded_file', fileType = whitch_file_type, filename = filename))
 
     return render_template('upload.html')
 
 
-#this needs to be fixed    
-@app.route('/<fileType>/<filename>')
+#this needs to be fixed
+@app.route('/uploads/<fileType>/<filename>')
 def uploaded_file(fileType, filename):
-    logging.info("hell" + fileType)
-    return send_from_directory(fileType, filename)
+    path_to_file = fileType + "/" + filename
+    logging.info("path: " + path_to_file)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], path_to_file)
 
 if __name__ == "__main__":
     app.run()
