a7f3a5a086f78eccaca5ce67fd99f7edb4b904bb
Brendan Murphy <thesimplekid@gmail.com>
2020-07-22 22:46:52 -0400

    fixed docker

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,7 @@ from models import db
 from routes import routes_for_flask
 
 logging.basicConfig(filename='app.log',
-					format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
+                    format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
 logging.warning("New Run Starts Here")
 
 current_directory = os.getcwd()
@@ -17,27 +17,6 @@ video_file_extentions = {'mov', 'mp4'}
 
 
 def create_app():
-<<<<<<< main
-	username = 'newuser'
-	password = 'newpassword'
-	location = 'mysql'
-	dbname = 'test_db'
-	app = Flask(__name__)
-	app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-	#  REVIEW:  where do we wanna save this and name it
-	app.config[
-		'SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{username}:{password}@{location}:3306/{dbname}?charset=utf8mb4"
-	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-	# Shows sql querys being made if having database issue set to true
-	app.config['SQLALCHEMY_ECHO'] = False
-	#app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
-	#  REVIEW:  this needs to be changed
-	app.secret_key = 'development key'
-	app.register_blueprint(routes_for_flask)
-	db.init_app(app)
-
-	return app
-=======
     username = 'newuser'
     password = 'newpassword'
     location = 'mysql'
@@ -54,7 +33,6 @@ def create_app():
     db.init_app(app)
 
     return app
->>>>>>> cross sections and such
 
 
 #  REVIEW: is having this here correct
@@ -64,11 +42,11 @@ db.create_all()
 
 
 def check_system():
-	if os.path.isdir(app.config['UPLOAD_FOLDER']) is False:
-		logging.info("no uploads folder")
-		os.mkdir(app.config['UPLOAD_FOLDER'])
-	if os.path.isdir(current_directory + '/static/img') is False:
-		os.mkdir(current_directory + '/static/img')
+    if os.path.isdir(app.config['UPLOAD_FOLDER']) is False:
+        logging.info("no uploads folder")
+        os.mkdir(app.config['UPLOAD_FOLDER'])
+    if os.path.isdir(current_directory + '/static/img') is False:
+        os.mkdir(current_directory + '/static/img')
 
 
 check_system()
