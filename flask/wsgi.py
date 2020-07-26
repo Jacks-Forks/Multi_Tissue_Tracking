ee17d304f2b8e3cfc97198ab8fc869398b7cb8e2
Jack <me@jackmurphy.nyc>
2020-07-26 11:10:17 -0400

    removed dash

--- flask/wsgi.py
+++ flask/wsgi.py
@@ -1,9 +1,4 @@
-from analysisFolder.dashboard import dasher as dashapp
 from app import app as flask_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
-application = DispatcherMiddleware(flask_app, {
-
-    '/dash': dashapp.server,
-
-})
+application = DispatcherMiddleware(flask_app)
